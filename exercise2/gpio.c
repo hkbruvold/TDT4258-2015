9ecbf44bc97e95dea27ea3f2e9cc4f7171e555d8
hkbruvold <hkbruvold@gmail.com>
2015-10-09 13:31:41 +0200

    Added code to enable buttons

--- exercise2/gpio.c
+++ exercise2/gpio.c
@@ -12,9 +12,15 @@ void setupGPIO()
      check efm32gg.h for other useful register definitions
   */
   *CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_GPIO; /* enable GPIO clock*/
+
+  /* Enable LEDs */
   *GPIO_PA_CTRL = 2;  /* set high drive strength */
   *GPIO_PA_MODEH = 0x55555555; /* set pins A8-15 as output */
   *GPIO_PA_DOUT = 0x0700; /* turn on LEDs D4-D8 (LEDs are active low) */
+  
+  /* Enable buttons */
+  *GPIO_PC_MODEL = 0x33333333; /* set pins to input with filter*/
+  *GPIO_PC_DOUT = 0xff; /* set pin to be pull-up */
 }
 
 
