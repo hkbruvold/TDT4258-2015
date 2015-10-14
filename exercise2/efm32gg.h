fd6a15182783aa6287d3caf6d267e6b8bf0d16c5
omsaggau <omsaggau@gmail.com>
2015-10-14 15:41:53 +0200

    LE defines

--- exercise2/efm32gg.h
+++ exercise2/efm32gg.h
@@ -53,12 +53,39 @@
 #define CMU_HFPERCLKEN0  ((volatile uint32_t*)(CMU_BASE2 + 0x044))
 #define CMU_CMD          ((volatile uint32_t*)(CMU_BASE2 + 0x024))
 
+#define CMU_OSCENCMD     ((volatile uint32_t*)(CMU_BASE2 + 0x020))
+#define CMU_LFCLKSEL     ((volatile uint32_t*)(CMU_BASE2 + 0x028))
+#define CMU_LFACLKEN0    ((volatile uint32_t*)(CMU_BASE2 + 0x058))
+#define CMU_LFBCLKEN0	 ((volatile uint32_t*)(CMU_BASE2 + 0x060))
+#define CMU_LFAPRESC0    ((volatile uint32_t*)(CMU_BASE2 + 0x068))
+
 #define CMU2_HFPERCLKEN0_DAC0   (1 << 17)
 #define CMU2_HFPERCLKEN0_PRS    (1 << 15)
 #define CMU2_HFPERCLKEN0_GPIO   (1 << 13)
 #define CMU2_HFPERCLKEN0_TIMER1 (1 << 6)
 
 #define CMU_HFCORECLKEN0_DMA (1 << 0)
+
+
+// LETIMER0
+
+#define LETIMER0_BASE 0x40082000 
+#define LETIMER0_CTRL   ((volatile uint32_t*)(LETIMER0_BASE + 0x00))
+#define LETIMER0_CMD    ((volatile uint32_t*)(LETIMER0_BASE + 0x04))
+#define LETIMER0_STATUS ((volatile uint32_t*)(LETIMER0_BASE + 0x08))
+#define LETIMER0_CNT    ((volatile uint32_t*)(LETIMER0_BASE + 0x0c))
+#define LETIMER0_COMP0  ((volatile uint32_t*)(LETIMER0_BASE + 0x10))
+#define LETIMER0_COMP1  ((volatile uint32_t*)(LETIMER0_BASE + 0x14))
+#define LETIMER0_REP0   ((volatile uint32_t*)(LETIMER0_BASE + 0x18))
+#define LETIMER0_REP1   ((volatile uint32_t*)(LETIMER0_BASE + 0x1c))
+#define LETIMER0_IFC    ((volatile uint32_t*)(LETIMER0_BASE + 0x28))
+#define LETIMER0_IEN    ((volatile uint32_t*)(LETIMER0_BASE + 0x2c))
+
+#define LETIMER0_CMD_START  = (1 << 0)
+#define LETIMER0_CMD_STOP   = (1 << 1)
+#define LETIMER0_CMD_CLEAR  = (1 << 2)
+#define LETIMER0_CTRL_BUFTOP    = (1 << 8)
+#define LETIMER0_CTRL_COMP0TOP  = (1 << 9)
 
 // TIMER1
 
