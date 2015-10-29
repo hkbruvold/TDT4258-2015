6137ef7283079deddd9b9f257dc6fbf16b873a9f
Håvard Pettersson <mail@haavard.me>
2015-10-29 16:27:55 +0100

    (gamepad) Add some empty functions.

--- exercise3/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ exercise3/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -6,6 +6,35 @@
 #include <linux/module.h>
 #include <linux/init.h>
 
+static int gamepad_open(struct inode *inode, struct file *filp)
+{
+    return 0;
+}
+
+static int gamepad_release(struct inode *inode, struct file *filp)
+{
+    return 0;
+}
+
+/*
+ * Note: The functions we introduce in this section and in "Using the ioctl
+ * Argument" in Chapter 5, "Enhanced Char Driver Operations" use some hidden
+ * magic to deal with page faults in the proper way even when the CPU is
+ * executing in kernel space.
+ */
+
+static ssize_t gamepad_read(struct file *filp, char __user *buff,
+                            size_t count, loff_t *offp)
+{
+    return 0;
+}
+
+static ssize_t gamepad_write(struct file *filp, const char __user *buff,
+                             size_t count, loff_t *offp)
+{
+    return 0;
+}
+
 /*
  * template_init - function to insert this module into kernel space
  *
@@ -15,10 +44,10 @@
  * Returns 0 if successfull, otherwise -1
  */
 
-static int __init template_init(void)
+static int __init gamepad_init(void)
 {
-	printk("Hello World, here is your module speaking\n");
-	return 0;
+    printk("Hello World, here is your module speaking\n");
+    return 0;
 }
 
 /*
@@ -28,14 +57,14 @@ static int __init template_init(void)
  * code from a running kernel
  */
 
-static void __exit template_cleanup(void)
+static void __exit gamepad_cleanup(void)
 {
-	 printk("Short life for a small module...\n");
+    printk("Short life for a small module...\n");
 }
 
-module_init(template_init);
-module_exit(template_cleanup);
+module_init(gamepad_init);
+module_exit(gamepad_cleanup);
 
-MODULE_DESCRIPTION("Small module, demo only, not very useful.");
+MODULE_DESCRIPTION("TDT4258 Gamepad driver");
 MODULE_LICENSE("GPL");
 
