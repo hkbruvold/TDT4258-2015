831fcbba35d0ae3956e518b5c8135b20039db2bb
hkbruvold <hkbruvold@gmail.com>
2015-11-02 13:25:59 +0100

    Added usage of drawrect

--- exercise3/local_src/game-1.0/game.c
+++ exercise3/local_src/game-1.0/game.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #include "framebuffer.h"
 
@@ -8,6 +9,12 @@ int main(int argc, char *argv[])
 	printf("Hello World, I'm game!\n");
 	
 	setupFB();
+	
+	color_t col;
+	col.red = 0b00000;
+	col.green = 0b111111;
+	col.blue = 0b00000;
+	drawRect(120, 100, 80, 50, &col);
 	
 	exit(EXIT_SUCCESS);
 }
