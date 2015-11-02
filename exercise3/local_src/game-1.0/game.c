5859918ca27ebe6597a7ef989b0e0523b6ba82c8
hkbruvold <hkbruvold@gmail.com>
2015-11-02 13:53:11 +0100

    drawing nice cube now, not boring rectangle

--- exercise3/local_src/game-1.0/game.c
+++ exercise3/local_src/game-1.0/game.c
@@ -10,8 +10,16 @@ int main(int argc, char *argv[])
 	
 	setupFB();
 	
-	uint16_t col = COLOR(0b11111, 0b000000, 0b11111);
-	drawRect(120, 100, 80, 50, &col);
+	uint16_t black = COLOR(0b00000, 0b000000, 0b00000);
+	uint16_t white = COLOR(0b11111, 0b111111, 0b11111);
+	drawRect(120, 100, 80, 80, &white);
+	drawRect(125, 105, 70, 70, &black);
+	drawRect(130, 110, 60, 60, &white);
+	drawRect(135, 115, 50, 50, &black);
+	drawRect(140, 120, 40, 40, &white);
+	drawRect(145, 125, 30, 30, &black);
+	drawRect(150, 130, 20, 20, &white);
+	drawRect(155, 135, 10, 10, &black);
 	
 	exit(EXIT_SUCCESS);
 }
