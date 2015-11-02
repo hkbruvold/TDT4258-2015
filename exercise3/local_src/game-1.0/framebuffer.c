4ab8d36be7825e492839602a4f8396bfb35dc5d6
hkbruvold <hkbruvold@gmail.com>
2015-11-02 11:49:56 +0100

    Added functions to setup and update framebuffer

---
+++ exercise3/local_src/game-1.0/framebuffer.c
@@ -0,0 +1,30 @@
+#include <stdint.h> // for integer types
+#include <sys/ioctl.h> // for driver control
+#include <linux/fb.h> // for framebuffer
+#include <sys/types.h> // for open()
+#include <sys/stat.h> // for open()
+#include <fcntl.h> // for open()
+#include <sys/mman.h> // for mmap
+
+uint16_t *screen;
+struct fb_copyarea rect;
+
+void setupFB()
+{
+	int fd = open("/dev/fb0", O_RDWR);
+	
+	screen = mmap(0, 320*240*2, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	
+	//screen[320*100+10] = 0xffff;
+
+        
+}
+
+void updateRect(int dx, int dy, int width, int height)
+{
+	rect.dx = dx;
+	rect.dy = dy;
+	rect.width = width;
+	rect.height = height;
+	ioctl(fd, 0x4680, &rect);
+}
---
+++ exercise3/local_src/game-1.0/framebuffer.h
@@ -0,0 +1,7 @@
+#ifndef FRAMEBUFFER_H
+#define FRAMEBUFFER_H
+
+void setupFB();
+void updateRect(int dx, int dy, int width, int height);
+
+#endif
