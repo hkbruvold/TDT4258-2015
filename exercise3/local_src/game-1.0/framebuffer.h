10f2c1d79798fb7f41fc92cd8f369bdc0c876901
hkbruvold <hkbruvold@gmail.com>
2015-11-02 13:47:57 +0100

    Added some comments

--- exercise3/local_src/game-1.0/framebuffer.h
+++ exercise3/local_src/game-1.0/framebuffer.h
@@ -1,11 +1,12 @@
 #ifndef FRAMEBUFFER_H
 #define FRAMEBUFFER_H
 
+// COLOR(5 bit red, 6 bit green, 5 bit blue)
 #define COLOR(red, green, blue) (red << 11) + (green << 5) + (blue)
 
-void setupFB();
-void updateRect(int dx, int dy, int width, int height);
-void clearScreen();
-void drawRect(int x0, int y0, int width, int height, uint16_t *col);
+void setupFB(); // used to load framebuffer file and clear screen
+void updateRect(int dx, int dy, int width, int height); // update screen on rectangle
+void clearScreen(); // clear the entire screen
+void drawRect(int x0, int y0, int width, int height, uint16_t *col); // draw rectangle with color
 
 #endif
