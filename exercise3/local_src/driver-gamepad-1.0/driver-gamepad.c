ff27efd191af0f83578ccb8b858c75a68c037310
Håvard Pettersson <mail@haavard.me>
2015-11-12 16:08:18 +0100

    asd

--- exercise3/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ exercise3/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -164,8 +164,8 @@ static int gpio_init(resource_size_t start_address)
     resource_size_t gpio_pc_address = start_address - 0x1B7;
     resource_size_t gpio_irq_address = start_address - 0xFF;
 
-    printk(KERN_DEBUG "Start address: %#llx", start_address);
-    printk(KERN_DEBUG "2 GPIO PC: %#llx IRQ: %#llx", gpio_pc_address, gpio_irq_address);
+    printk(KERN_DEBUG "Start address: %#lx", start_address);
+    printk(KERN_DEBUG "2 GPIO PC: %#lx IRQ: %#lx", gpio_pc_address, gpio_irq_address);
 
     // request exclusive access to the GPIO port C memory region
     if (request_mem_region(gpio_pc_address, GPIO_PC_LENGTH, DEVICE_NAME) == NULL)
