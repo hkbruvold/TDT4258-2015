9d624de181747c4ef4a14eb60b579ed36a2dbb89
Håvard Pettersson <mail@haavard.me>
2015-11-12 16:30:21 +0100

    whitespace

--- exercise3/local_src/driver-gamepad-1.0/efm32gg.h
+++ exercise3/local_src/driver-gamepad-1.0/efm32gg.h
@@ -5,9 +5,9 @@
 
 // platform device GPIO start address: 0x400061FF
 
-#define GPIO_MEM_INDEX 0
-#define GPIO_EVEN_IRQ_INDEX 0
-#define GPIO_ODD_IRQ_INDEX 1
+#define GPIO_MEM_INDEX       0
+#define GPIO_EVEN_IRQ_INDEX  0
+#define GPIO_ODD_IRQ_INDEX   1
 
 // #define GPIO_PC_BASE     0x40006048
 #define GPIO_PC_LENGTH   (GPIO_PINLOCKN + 4)
